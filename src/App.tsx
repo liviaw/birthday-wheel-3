a302c05a60767f70296a2a82c9a5d6be85c3f831
Jessica Claire Edwards <jessica.e@canva.com>
2021-07-23 12:05:15 +1000

    fix paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Quiz } from './components/quiz';
 import styles from './app.module.css';
 
 function App() {
   return (
     <div className={styles.app}>
-      <header className={styles.header}>
-      <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          hi
-        </a>
-      </header>
+      <Quiz />
     </div>
   );
 }
